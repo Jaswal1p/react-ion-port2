b7c6f10cfc4a9fd6e9e003ac1f73105c3083a277
Jaswal1p <Jaswal1p@yahoo.com>
2023-03-13 12:15:31 -0400

    pushing initial react code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lets make some cool stuff!
+          
         </p>
         <a
           className="App-link"
